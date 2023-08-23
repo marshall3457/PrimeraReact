3ab530f20c256ee38a919920e1d62fb0fb69022b
Bruno <carlitos078955@gmail.com>
2023-08-22 20:57:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Header from './componentes/Header/Header';
+import Formulario from './componentes/Formulario/Formulario';
+import MiOrg from './componentes/MiOrg';
+import Equipo from './componentes/Equipo';
+
+
 
 function App() {
+  const [mostrarFormulario, actualizarMostrar] = useState(false);
+  const [colaboradores, actualizarColaboradores] = useState([]);
+
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarFormulario);
+  }
+
+const registrarColaborador = (colaborador) => {
+  console.log("Nuevo Colaborador", colaborador)
+  actualizarColaboradores([...colaboradores, colaborador]) //esta clonando el arreglo y le esta agregando el colaborador a la lista.El resultado es un nuevo arreglo que contiene todos los colaboradores originales más el nuevo colaborador.
+}
+
+  //Lista de equipos
+
+  const equipos = [
+    {
+      titulo: "Programación",
+      colorPrimario: "#57C278",
+      colorSecundario: "#D9F7E9"
+    },
+    {
+      titulo: "Front End",
+      colorPrimario: "#82CFFA",
+      colorSecundario: "#E8F8FF"
+    },
+    {
+      titulo: "Data Science",
+      colorPrimario: "#A6D157",
+      colorSecundario: "#F0F8E2"
+    },
+    {
+      titulo: "Devops",
+      colorPrimario: "#E06B69",
+      colorSecundario: "#FDE7E8"
+    },
+    {
+      titulo: "UX y Diseño",
+      colorPrimario: "#DB6EBF",
+      colorSecundario: "#FAE9F5"
+    },
+    {
+      titulo: "Móvil",
+      colorPrimario: "#FFBA05",
+      colorSecundario: "#FFF5D9"
+    },
+    {
+      titulo: "Innovación y  Gestión",
+      colorPrimario: "#FF8A29",
+      colorSecundario: "#FFEEDF"
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      {/* { mostrarFormulario ? <Formulario /> : <></>} */}
+      {
+        mostrarFormulario && <Formulario 
+          equipos={equipos.map( (equipo) => equipo.titulo)}
+          registrarColaborador={registrarColaborador}
+        />
+      }
+
+      <MiOrg  cambiarMostrar={cambiarMostrar}/>
+
+
+      {
+        equipos.map( (equipo) => <Equipo datos={equipo} key={equipo.titulo}/>) //return implicito
+      }
     </div>
   );
 }
