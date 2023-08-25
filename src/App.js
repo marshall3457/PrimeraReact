eb7a4df7429e71528a726d9464d229580bcaca05
Bruno <carlitos078955@gmail.com>
2023-08-24 20:33:23 -0500

    Agregando mas funciones

--- src/App.js
+++ src/App.js
@@ -1,64 +1,124 @@
 import { useState } from 'react';
+import { v4 as uuid} from "uuid"
 import './App.css';
 import Header from './componentes/Header/Header';
 import Formulario from './componentes/Formulario/Formulario';
 import MiOrg from './componentes/MiOrg';
 import Equipo from './componentes/Equipo';
-
-
+import Footer from './componentes/Footer';
 
 function App() {
   const [mostrarFormulario, actualizarMostrar] = useState(false);
-  const [colaboradores, actualizarColaboradores] = useState([]);
+  const [colaboradores, actualizarColaboradores] = useState([{
+    id: uuid(),
+    equipo: "Front End",
+    foto: "https://github.com/harlandlohora.png",
+    nombre: "Harland Lohora",
+    puesto: "Instructor"
+  },
+  {
+    id: uuid(),
+    equipo: "Programación",
+    foto: "https://github.com/genesysaluralatam.png",
+    nombre: "Genesys Rondón",
+    puesto: "Desarrolladora de software e instructora"
+  },
+  {
+    id: uuid(),
+    equipo: "UX y Diseño",
+    foto: "https://github.com/JeanmarieAluraLatam.png",
+    nombre: "Jeanmarie Quijada",
+    puesto: "Instructora en Alura Latam"
+  },
+  {
+    id: uuid(),
+    equipo: "Programación",
+    foto: "https://github.com/christianpva.png",
+    nombre: "Christian Velasco",
+    puesto: "Head de Alura e Instructor"
+  },
+  {
+    id: uuid(),
+    equipo: "Innovación y Gestión",
+    foto: "https://github.com/JoseDarioGonzalezCha.png",
+    nombre: "Jose Gonzalez",
+    puesto: "Dev FullStack"
+  }]);
 
-  const cambiarMostrar = () => {
-    actualizarMostrar(!mostrarFormulario);
-  }
-
-const registrarColaborador = (colaborador) => {
-  console.log("Nuevo Colaborador", colaborador)
-  actualizarColaboradores([...colaboradores, colaborador]) //esta clonando el arreglo y le esta agregando el colaborador a la lista.El resultado es un nuevo arreglo que contiene todos los colaboradores originales más el nuevo colaborador.
-}
-
-  //Lista de equipos
-
-  const equipos = [
+  const [equipos, actualizarEquipos] = useState([
     {
+      id: uuid(),
       titulo: "Programación",
       colorPrimario: "#57C278",
       colorSecundario: "#D9F7E9"
     },
     {
+      id: uuid(),
       titulo: "Front End",
       colorPrimario: "#82CFFA",
       colorSecundario: "#E8F8FF"
     },
     {
+      id: uuid(),
       titulo: "Data Science",
       colorPrimario: "#A6D157",
       colorSecundario: "#F0F8E2"
     },
     {
+      id: uuid(),
       titulo: "Devops",
       colorPrimario: "#E06B69",
       colorSecundario: "#FDE7E8"
     },
     {
+      id: uuid(),
       titulo: "UX y Diseño",
       colorPrimario: "#DB6EBF",
       colorSecundario: "#FAE9F5"
     },
     {
+      id: uuid(),
       titulo: "Móvil",
       colorPrimario: "#FFBA05",
       colorSecundario: "#FFF5D9"
     },
     {
+      id: uuid(),
       titulo: "Innovación y  Gestión",
       colorPrimario: "#FF8A29",
       colorSecundario: "#FFEEDF"
     }
-  ]
+  ])
+
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarFormulario);
+  }
+
+  const registrarColaborador = (colaborador) => {
+    console.log("Nuevo Colaborador", colaborador)
+    actualizarColaboradores([...colaboradores, colaborador]) //esta clonando el arreglo y le esta agregando el colaborador a la lista.El resultado es un nuevo arreglo que contiene todos los colaboradores originales más el nuevo colaborador.
+  }
+
+
+  const eliminarColaborador = () => {
+    console.log("Eliminar colaborador");
+
+  }
+
+  const actualizarColor = (color,id ) =>{
+    console.log("Actualizar:", color, id);
+    const equiposActualizados = equipos.map((equipo) => {
+      if(equipo.id === id){
+        equipo.colorPrimario = color
+      }
+
+      return equipo
+    })
+
+    actualizarEquipos(equiposActualizados)
+  }
+  //Lista de equipos
+
 
   return (
     <div>
@@ -75,8 +135,17 @@ const registrarColaborador = (colaborador) => {
 
 
       {
-        equipos.map( (equipo) => <Equipo datos={equipo} key={equipo.titulo}/>) //return implicito
+        equipos.map( (equipo) => <Equipo 
+          datos={equipo} 
+          key={equipo.titulo}
+          colaboradores={colaboradores.filter(colaborador => colaborador.equipo === equipo.titulo)}
+          eliminarColaborador={eliminarColaborador} 
+          actualizarColor = {actualizarColor} 
+        />
+          ) //return implicito
       }
+
+      <Footer />
     </div>
   );
 }
--- src/componentes/Equipo/index.js
+++ src/componentes/Equipo/index.js
@@ -1,26 +1,43 @@
 import "./Equipo.css"
 import Colaborador from "../Colaborador";
+import hexToRgba from 'hex-to-rgba';
+
 
 const Equipo = (props) => {
-    const { colorPrimario , colorSecundario, titulo} = props.datos;
+    const { colorPrimario , colorSecundario, titulo, id} = props.datos;
+    const { colaboradores , eliminarColaborador, actualizarColor } = props;
 
     const obj = {
-        backgroundColor: colorSecundario
+        backgroundColor: hexToRgba(colorPrimario, 0.6)
     }
-
     const estiloTitulo = {borderColor: colorPrimario}
 
-    return <section className="equipo" style={obj}>
+    return <>
+    { colaboradores.length > 0 &&
+        <section className="equipo" style={obj}>
+        <input 
+            type="color"
+            className="input-color"
+            value={colorPrimario}
+            onChange={(evento) => {
+                actualizarColor(evento.target.value, id)
+            }}
+        
+        />
         <h3 style={estiloTitulo}>{titulo}</h3>
         <div className="colaboradores">
-            <Colaborador />
-            <Colaborador />
-            <Colaborador />
-            <Colaborador />
-
+            {
+                colaboradores.map( (colaborador, index) => <Colaborador 
+                    datos={colaborador} 
+                    key={index} 
+                    colorPrimario={colorPrimario}
+                    eliminarColaborador={eliminarColaborador}
+                />)
+            }
         </div>
     </section>
-
+    }
+    </>
 }
 
 
